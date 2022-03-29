64326e90fb7f4ccd18e3f85da9cebe3a72eb2ec4
callmepazuzu <102613427+callmepazuzu@users.noreply.github.com>
2022-03-30 00:05:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+const  CONFIG  =  {
+    titleWeb : "SOS" ,
+    introTitle : 'HELLO' ,
+    introDesc : Pazuzu xin chào quý khách` ,
+    btnIntro : 'Ấn tiếp đi nè' ,
+    title : 'Em ăn cơm chưa? ngày hôm nay của em thế nào =))' ,
+    desc : 'Em muốn chúng ta đi đâu chơi nè ??' ,
+    btn Có : 'Đi ks lun' ,
+    btnNo : 'hong bé ơi' ,
+    question : 'Vậy là đi nhà nghỉ thiệt luôn hả :)))' ,
+    btnReply : 'IB liền cho Anh' ,
+    reply : 'Dạ, mình đi ks ạ :)))' ,
+    mess : 'Anh biết mà :)) ok iêm anh qua liền á ' ,
+    messDesc : 'Nhấn vào ok để ib liền cho anh nè " ,
+    btn Chấp nhận : 'Oki' ,
+    messLink : 'https://github.com/callmepazuzu/pazuzu'//https://www.facebook.com/messages/t/100075447270628
 }
